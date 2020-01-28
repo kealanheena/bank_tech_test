911abc774d5a8a99281cb6377a77ad91aff46007
kealanheena <kealanheena@gmail.com>
2020-01-28 14:38:32 +0000

    Fix: Change initialize and restart return method

--- lib/bank_data.rb
+++ lib/bank_data.rb
@@ -1,12 +1,18 @@
 class BankData
-  def initialize(money)
-    @statement = {
-      date: Time.now.strftime('%d/%m/%Y'),
-      amount: money
-    }
+
+  DEFAULT_BALANCE = sprintf("%.2f", 0)
+  attr_reader :balance
+
+  def initialize(balance = DEFAULT_BALANCE)
+    @balance = DEFAULT_BALANCE
   end
 
-  def return
-    @statement
-  end
+  # @statement = {
+    #   date: Time.now.strftime('%d/%m/%Y at %H:%M'),
+    #   amount: money
+    # }
+
+  # def return
+  #   @statement
+  # end
 end
--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -1,15 +1,32 @@
 require 'bank_data'
 
 describe BankData do
-  describe '#return' do
-    before :all do
-      @data = BankData.new(100)
-    end
-    it 'returns the date of the transaction' do
-      expect(@data.return).to include(date: Time.now.strftime('%d/%m/%Y'))
-    end
-    it 'returns the amount of money deposited' do
-      expect(@data.return).to include(amount: 100)
+
+  before :all do
+    @bank_data = BankData.new(100)
+    @time = Time.now.strftime('%d/%m/%Y at %H:%M')
+  end
+
+  describe '#initialize' do
+    
+    it 'should be initialized with a default balance of 0' do
+      data = BankData.new
+      expect(data.balance).to eq sprintf("%.2f", 0)
     end
   end
+
+  # describe '#return' do
+
+  #   it 'returns the date of the transaction' do
+  #     expect(@data.return).to include(date: @time)
+  #   end
+
+  #   it 'returns the amount of money deposited' do
+  #     expect(@data.return).to include(deposit: 100)
+  #   end
+
+  #   it 'returns the amount of money withdrawn' do
+  #     expect(@data.return).to include(withdrawl: 100)
+  #   end
+  # end
 end
