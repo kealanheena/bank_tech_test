5d7d0481e729cbdc2da1f0f8ae1637285e84cb2c
kealanheena <kealanheena@gmail.com>
2020-01-28 14:49:01 +0000

    Chore: Add test for checking that you can initialize with different amounts

--- lib/bank_data.rb
+++ lib/bank_data.rb
@@ -1,18 +1,17 @@
 class BankData
-
-  DEFAULT_BALANCE = sprintf("%.2f", 0)
+  DEFAULT_BALANCE = 0
   attr_reader :balance
 
   def initialize(balance = DEFAULT_BALANCE)
-    @balance = DEFAULT_BALANCE
+    @balance = format('%.2f', balance)
   end
 
-  # @statement = {
-    #   date: Time.now.strftime('%d/%m/%Y at %H:%M'),
-    #   amount: money
-    # }
+#  @statement = {
+#   date: Time.now.strftime('%d/%m/%Y at %H:%M'),
+#   amount: money
+# }
 
-  # def return
-  #   @statement
+# def return
+#   @statement
   # end
 end
--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -1,18 +1,27 @@
 require 'bank_data'
 
 describe BankData do
-
-  before :all do
-    @bank_data = BankData.new(100)
-    @time = Time.now.strftime('%d/%m/%Y at %H:%M')
-  end
-
   describe '#initialize' do
-    
     it 'should be initialized with a default balance of 0' do
       data = BankData.new
-      expect(data.balance).to eq sprintf("%.2f", 0)
+      expect(data.balance).to eq format('%.2f', 0)
     end
+
+    it 'should be able to initialized with a balance of 100' do
+      data = BankData.new(100)
+      expect(data.balance).to eq format('%.2f', 100)
+    end
+  end
+
+  describe '#deposit' do
+    before :all do
+      @bank_data = BankData.new(100)
+      @time = Time.now.strftime('%d/%m/%Y at %H:%M')
+    end
+
+    # it 'returns the date of the transaction' do
+    #   expect().to include(date: @time)
+    # end
   end
 
   # describe '#return' do
@@ -29,4 +38,5 @@ describe BankData do
   #     expect(@data.return).to include(withdrawl: 100)
   #   end
   # end
+
 end
